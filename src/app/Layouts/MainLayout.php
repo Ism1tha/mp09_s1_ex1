7ca5a97bb0595c3da8be7baeabc559be1bc0fb62
Ism1tha <xgalan@hotmail.com>
2024-10-07 17:04:25 +0200

    Main CSS base file and add it on Main and Error Layout

--- src/app/Layouts/MainLayout.php
+++ src/app/Layouts/MainLayout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $_ENV['APP_NAME'] . ' - ' . $title ?></title>
+    <link rel="stylesheet" href="/public/assets/css/main.css">
     <script src="/public/assets/js/main.js"></script>
 </head>
 <header>
