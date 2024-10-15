ba12ba1be350ac4d8866182908873ca9805a49f9
Ism1tha <xgalan@hotmail.com>
2024-10-15 18:10:05 +0200

    Error view structure and styles

--- src/app/Views/ErrorView.php
+++ src/app/Views/ErrorView.php
@@ -1,2 +1,4 @@
-Error: <?php echo $errorCode; ?> - <?php echo $errorMessage; ?>
-<a href="/">Volver a inicio</a>
+<div class="error-container">
+    <p>Error: <?php echo $errorCode; ?> - <?php echo $errorMessage; ?></p>
+    <a href="/">Volver a inicio</a>
+</div>
